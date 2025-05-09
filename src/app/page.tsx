b3ff319a7d9e7ca7a18deeed32bbebc1751e55ac
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-05-09 08:24:17 +0400

    addedloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import HomePage from "@/home/page";
+import { BounceLoader } from "react-spinners";
 
+export default function Page() {
+  const [loading, setLoading] = useState(true);
 
-export default function page() {
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <HomePage />
+    <div style={{ position: "relative" }}>
+      {/* LOADER OVERLAY */}
+      {loading && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "#ffffff",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 9999,
+          }}
+        >
+          <BounceLoader color="#002aff" />
+        </div>
+      )}
+
+      {/* PAGE CONTENT */}
+      <div
+        style={{ opacity: loading ? 0 : 1, transition: "opacity 0.5s ease" }}
+      >
+        <HomePage />
+      </div>
     </div>
   );
 }
