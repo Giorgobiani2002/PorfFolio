af6a779f1dc4162efdd1264d26eed7f1e278cd44
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-10-22 12:35:50 +0400

    added chatbot

---
+++ src/app/components/N8nChat.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+import "@n8n/chat/style.css";
+import { createChat } from "@n8n/chat";
+
+export default function N8nChat() {
+  useEffect(() => {
+    createChat({
+      webhookUrl:
+        "http://localhost:5678/webhook/5219a7a3-04d7-4d02-9a25-154686fdcdda/chat",
+      mode: "window",
+      title: "Chat with Support",
+      theme: { primaryColor: "#2563eb" },
+    } as any);
+  }, []);
+
+  return <div></div>;
+}
